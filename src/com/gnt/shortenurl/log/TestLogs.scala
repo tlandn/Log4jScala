e18605f30ecccfb9598833866147e80730b85f6d
LanTra <lantra@lantra-desktop.(none)>
2010-06-14 16:11:10 +0700

    First commit

---
+++ src/com/gnt/shortenurl/log/TestLogs.scala
@@ -0,0 +1,11 @@
+package com.gnt.shortenurl.log
+
+object TestLogs extends Application with Logs {
+   //info("This is a very.complicated log message using ConsoleAppender and RollingFileAppender")
+
+  info("Using default property file")
+
+  //setPropertyFile("/home/lantra/IdeaProjects/Log4jScala/testLog4j/log4j.properties")
+  //info("Using different property file")
+
+}
